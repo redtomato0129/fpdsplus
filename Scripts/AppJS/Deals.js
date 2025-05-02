889225da3450a635fa37368a286b8fb9a1d41d71
Ibrahim298 <h.m.ibrahim298@gmail.com>
2025-05-02 17:54:22 +0500

    crm deal modifications

--- Scripts/AppJS/Deals.js
+++ Scripts/AppJS/Deals.js
@@ -1280,12 +1280,13 @@ function GetDeals(searchtext, pageNo) {
                 filldata = filldata +
                     '<tr style="text-align:center" id="' + Deal_ID + '">' +
                     '<td >' + '<a class="fas fa-edit text-centre" target="_blank" href="/CrmDeals/AddDeal?dealId=' + Deal_ID + '" > ' + '</a>' + '</td > ' +
+                    '<td style="none: " FontAwesome";"> ' + organization_name + '</td>' +
                     '<td style="font-family: ;white-space: break-spaces;"> ' + title + '</td > ' +
                     '<td style="font-family: " FontAwesome";" >' + rfp_release_date + '</td>' +
+                    '<td style="font-family: " FontAwesome";"> ' + ` ${potential_award_amount ? "$" + potential_award_amount.toLocaleString('en') : ''}` + '</td>' +
                     '<td  style="font-family: " FontAwesome";">' + status + '</td > ' +
                     '<td style="font-family: " FontAwesome";">' + stage + '</td > ' +
                     '<td style="none: " FontAwesome";"> ' + priority + '</td>' +
-                    '<td style="none: " FontAwesome";"> ' + organization_name + '</td>' +
                     '<td class="text-center" style="justify-items: anchor-center;"><div class="d-flex  border-0"> <i class="fa-solid fa-eye mr-2"  onclick="openAttchViewPeopleModal(`' + Deal_ID + '`)" style="color: #571C7A;"></i> <i class="fa-solid fa-circle-plus" style="color: #08A742;"  onclick="openAttchPeopleModal(`' + Deal_ID + '`,`' + title + '`,`' + status + '`,`' + rfp_release_date + '`)" ></i></div></td>' +
                     '<td class="text-center" style="justify-items: anchor-center;"><div class="d-flex  border-0"> <i class="fa-solid fa-eye mr-2"  onclick="openViewGovtContactsModal(`' + Deal_ID + '`)" style="color: #571C7A;"></i> <i class="fa-solid fa-circle-plus" style="color: #08A742;"  onclick="openAttachGovtContactsModal(`' + Deal_ID + '`)" ></i></div></td>' +
                     '<td dealId=' + Deal_ID + ' class="dt-control" style="justify-items: anchor-center;"><div class="d-flex border-0" style="margin-right: 11px;"><i class="fa-solid fa-eye mr-2"  style="color: #571C7A;"></i> <i class="fa-solid fa-circle-plus" style="color: #08A742;"></i></div></td>' +
